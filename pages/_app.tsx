2bcb9e7334db5f2f01ac69d32dbbadc824135b84
tzovkata <teodor.emil.yordanov@gmail.com>
2025-07-06 14:57:30 +0100

    asda

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: { Component: any; pageProps: any }) {
   return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      {/* Debug test - this should show red background */}
+      <div style={{backgroundColor: 'red', color: 'white', padding: '20px', textAlign: 'center'}}>
+        <h1>DEBUG: If you see this red box, React is working</h1>
+      </div>
+      
+      {/* Tailwind test */}
+      <div className="bg-blue-500 text-white p-4 text-center">
+        <h1 className="text-3xl font-bold">TAILWIND TEST: This should be blue background</h1>
+      </div>
+      
       <DigitalPhotographyConference />
     </>
   );
